6eeb1319359663aebdca813291632c5a07a964bb
Joachim1989 <143495304+Joachim1989@users.noreply.github.com>
2026-08-03 13:40:22 +0200

    Add files via upload

---
+++ sw (2).js
@@ -0,0 +1,30 @@
+const CACHE = 'pixel-papa-live-v2';
+const FILES = [
+  './',
+  './pixel-papa-live.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-mask-192.png',
+  './icon-mask-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.hostname.includes('generativelanguage')) return;
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (res && res.ok && url.origin === location.origin) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match('./pixel-papa-live.html')))
+  );
+});
